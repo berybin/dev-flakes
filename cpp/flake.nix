3f3d64dae967f7d61fb77e32b36967990343872e
Jay @ Automated <me@jayparry.dev>
2026-01-07 13:39:09 +1100

    feat(cpp): add c++ dev flake

---
+++ cpp/flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "A nix flake-based development environment for C++";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+
+    in
+    {
+
+      devShells = forEachSystem (
+        { pkgs }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+
+                # Override stdenv in order to change compiler:
+                # stdenv = pkgs.clangStdenv;
+              }
+
+              {
+                packages =
+                  with pkgs;
+                  [
+                    clang-tools
+                    codespell
+                    cppcheck
+                    vcpkg
+                    vcpkg-tool
+                  ]
+                  ++ (if stdenv.hostPlatform.system == "aarch64-darwin" then [ ] else [ gdb ]);
+              };
+        }
+      );
+    };
+}
