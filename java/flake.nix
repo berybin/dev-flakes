c71f15b3dc5fd404b466f5aabd732ab5535c6d5f
Jay @ Automated <me@jayparry.dev>
2026-03-05 10:47:30 +1100

    refactor: pin dev-flakes to stable nixpkgs channel

--- java/flake.nix
+++ java/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A nix flake-based development environment for java";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
   outputs =
     { self, nixpkgs }:
